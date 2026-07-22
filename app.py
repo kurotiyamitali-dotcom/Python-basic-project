1f441ecf6ae3970874ceb8bd00052f33cb313032
kurotiyamitali-dotcom <kurotiyamitali@gmail.com>
2026-07-23 00:21:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+
+model = joblib.load("heart_model.pkl")
+columns = joblib.load("columns.pkl")
+
+st.set_page_config(
+    page_title="Heart Disease Prediction", 
+    layout="centered"
+)
+
+st.title("❤️ Heart Disease Prediction")
+
+
+age = st.number_input(
+    "Age",
+    min_value=1,
+    max_value=120, 
+    value=50
+)
+sex = st.selectbox(
+    "Sex", 
+    [0, 1]
+)
+cp = st.selectbox(
+    "Chest Pain Type (cp)", 
+    [0, 1, 2, 3]
+)
+trestbps = st.number_input(
+    "Resting Blood Pressure (trestbps)", 
+    value=120
+)
+chol = st.number_input(
+    "Cholesterol (chol)", 
+    value=200
+)
+fbs = st.selectbox(
+    "Fasting Blood Sugar (fbs)", 
+    [0, 1]
+)
+restecg = st.selectbox(
+    "Rest ECG (restecg)", 
+    [0, 1, 2]
+)
+thalach = st.number_input(
+    "Maximum Heart Rate (thalach)", 
+    value=150
+)
+exang = st.selectbox(
+    "Exercise Induced Angina (exang)", 
+    [0, 1]
+)
+oldpeak = st.number_input(
+    "Old Peak", 
+    value=1.0
+)
+slope = st.selectbox(
+    "Slope", 
+    [0, 1, 2]
+)
+ca = st.selectbox(
+    "Number of Major Vessels (ca)", 
+    [0, 1, 2, 3, 4]
+)
+thal = st.selectbox(
+    "Thal", 
+    [0, 1, 2, 3]
+)
+
+if st.button("Predict"):
+
+    input_data = pd.DataFrame([[age, sex, cp, trestbps, chol, fbs,
+                                restecg, thalach, exang, oldpeak,
+                                slope, ca, thal]], columns=columns)
+
+    prediction = model.predict(input_data)
+    st.write("Prediction:", prediction)
+    if prediction[0] == 1:
+        st.error("❤️ Heart Disease Detected")
+    else:
+        st.success("💚 No Heart Disease")
